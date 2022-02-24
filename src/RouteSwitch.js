0bcecdcd1e7d02c851f9246d605c27942385a374
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 01:03:14 -0500

    create route path for each shop item - craete a not found route that only renders if the requested route is not found

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -3,6 +3,7 @@ import Cart from "./Cart";
 import Home from "./Home";
 import Nav from "./Nav";
 import Shop from "./Shop";
+import ShopItem from "./ShopItem";
 
 const RouteSwitch = () => {
   return (
@@ -11,10 +12,18 @@ const RouteSwitch = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:id" element={<ShopItem />} />
         <Route path="/cart" element={<Cart />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>Page Not Found</p>
+            </main>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 };
-
 export default RouteSwitch;
