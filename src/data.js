71d3b72a816c551262d437380f1eb9d6a24c54ca
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 09:24:33 -0500

    add ids to each item in data file

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 const products = [
   {
+    id: 1,
     img: "https://cdni.llbean.net/is/image/wim/510981_1155_41?hei=1095&wid=950&resMode=sharp2&defaultImage=llbprod/A0211793_2",
     price: 43.95,
     name: "Graffi Cotton Sweater",
@@ -7,6 +8,7 @@ const products = [
       "Artsy shirt made of love and hard work. Comforts the skin and fits true to size.",
   },
   {
+    id: 2,
     img: "https://www.chloe.com/product_image/54176787vq/f/w1080.jpg",
     price: 76.82,
     name: "Graffi Cotton Trousers",
@@ -15,6 +17,7 @@ const products = [
   },
 
   {
+    id: 3,
     img: "https://cdn.shopify.com/s/files/1/0193/6253/products/2706352897_1600x.jpg?v=1638487619",
     price: 39.85,
     name: "Graffi Circ Shades",
