f64e9b14401c94bed3e8d85f7821382160936a5d
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 09:22:09 -0500

    use context to send cart info to each route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,28 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cart from "./Cart";
+import Cart from "./Cart/Cart";
 import Home from "./Home";
 import Nav from "./Nav";
 import Shop from "./Shop";
 import ShopItem from "./ShopItem";
+import CartContext from "./Cart/CartContext";
+
+let cart = [];
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/:id" element={<ShopItem />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>Page Not Found</p>
-            </main>
-          }
-        />
-      </Routes>
+      <CartContext.Provider value={cart}>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:id" element={<ShopItem />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Page Not Found</p>
+              </main>
+            }
+          />
+        </Routes>
+      </CartContext.Provider>
     </BrowserRouter>
   );
 };
