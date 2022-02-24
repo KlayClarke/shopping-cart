5109daaf3ef73833701150f50a437f6cbf78975f
Klay Clarke <klayaclarke@gmail.com>
2022-02-23 21:54:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
+
+// TODO
+// create header with shopping cart icon
+// create homepage, items page, and shopping cart routes
+// if shopping cart is not empty display number of items next to icon
+// when user clicks shopping cart icon, display modal
+// when user clicks shopping item, display modal that includes:
+//// item price, item pictures, ability to add to cart and increment amount of said item in cart
